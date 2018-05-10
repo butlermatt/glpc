1d80d8e9990221ded267f353e1c37eb2ce7655e3
Matthew Butler <butler.matthew@gmail.com>
2018-05-09 22:31:10 -0300

    define Environment methods.

--- object/environment.go
+++ object/environment.go
@@ -1,13 +1,25 @@
 package object
 
-type Environment struct {
-	parent *Environment
-	m map[string]Object
-}
+import "github.com/butlermatt/glpc/lexer"
 
-const __GlobalEnv__ string = "__global__"
+const __GlobalEnv__ = "__global__"
 
 var fileScopes map[string]*Environment = make(map[string]*Environment)
+
+func GetGlobal() *Environment {
+	if env, ok := fileScopes[__GlobalEnv__]; ok {
+		return env
+	}
+
+	env := NewEnvironment(__GlobalEnv__)
+	// TODO: Populate builtin functions.
+	return env
+}
+
+type Environment struct {
+	parent *Environment
+	m      map[string]Object
+}
 
 func NewEnvironment(filename string) *Environment {
 	env := &Environment{m: make(map[string]Object)}
@@ -19,12 +31,54 @@ func NewEnclosedEnvironment(enclosing *Environment) *Environment {
 	return &Environment{parent: enclosing, m: make(map[string]Object)}
 }
 
-func GetGlobal() *Environment {
-	if env, ok := fileScopes[__GlobalEnv__]; ok {
-		return env
+func (e *Environment) Define(name *lexer.Token, value Object) error {
+	if _, ok := e.m[name.Lexeme]; ok {
+		return NewRuntimeError(name, "Variable has already been declared.")
 	}
 
-	env := NewEnvironment(__GlobalEnv__)
-	// TODO: Populate builtin functions.
-	return env
+	e.m[name.Lexeme] = value
+	return nil
+}
+
+func (e *Environment) Get(name *lexer.Token) (Object, error) {
+	v, ok := e.m[name.Lexeme]
+	if ok {
+		return v, nil
+	}
+
+	if e.parent != nil {
+		return e.parent.Get(name)
+	}
+
+	return nil, NewRuntimeError(name, "Undefined variable.")
+}
+
+func (e *Environment) GetAt(distance int, name *lexer.Token) (Object, error) {
+	env := e
+	for i := 0; i < distance; i++ {
+		env = e.parent
+	}
+
+	return env.Get(name)
+}
+
+func (e *Environment) Assign(name *lexer.Token, value Object) error {
+	if _, ok := e.m[name.Lexeme]; ok {
+		e.m[name.Lexeme] = value
+	}
+
+	if e.parent != nil {
+		return e.parent.Assign(name, value)
+	}
+
+	return NewRuntimeError(name, "Undefined variable.")
+}
+
+func (e *Environment) AssignAt(distance int, name *lexer.Token, value Object) error {
+	env := e
+	for i := 0; i < distance; i++ {
+		env = env.parent
+	}
+
+	return env.Assign(name, value)
 }
